821133bbaa50d9c4d87a2c0295326ad9f7d48fe6
apriliaputri04 <apriliaputri8057@gmail.com>
2025-04-23 00:54:06 +0700

    +migration

---
+++ database/migrations/2025_04_22_021702_create_m_barang_table.php
@@ -0,0 +1,30 @@
+<?php
+ 
+ use Illuminate\Database\Migrations\Migration;
+ use Illuminate\Database\Schema\Blueprint;
+ use Illuminate\Support\Facades\Schema;
+ 
+ return new class extends Migration
+ {
+     /**
+      * Run the migrations.
+      */
+     public function up(): void
+     {
+         Schema::create('m_barang', function (Blueprint $table) {
+             $table->id(); // primary key auto-increment
+             $table->string('nama_barang'); // nama barang
+             $table->decimal('harga', 10, 2); // harga dengan 2 angka di belakang koma
+             $table->integer('stok'); // stok barang
+             $table->timestamps(); // created_at dan updated_at
+         });
+     }
+ 
+     /**
+      * Reverse the migrations.
+      */
+     public function down(): void
+     {
+         Schema::dropIfExists('m_barang');
+     }
+ };
